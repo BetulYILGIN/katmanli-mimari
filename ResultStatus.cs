77c4b3bba414aef169ede3486def0d8eeffa9104
Betül YILGIN <betullyilgin@gmail.com>
2023-02-06 00:08:19 +0300

    Add files via upload

---
+++ ResultStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.ComplexTypes
+{
+    public enum ResultStatus
+    {
+        succses = 0,
+        Error = 1,
+        Warning = 2,
+        Info = 3,
+        Success = 4
+    }
+}
